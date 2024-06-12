19bfacfcf9914a04b59900ca77674c040b2f09dc
MUSTAFA YAVAŞ <mustafa.yavas@ogr.bandirma.edu.tr>
2024-06-12 13:20:04 +0300

    proje eklendi

---
+++ ehirPlakaKoduBulma/app/src/main/java/com/example/ehirplakakodubulma/ActivityMainBinding.kt
@@ -0,0 +1,29 @@
+package com.example.ehirplakakodubulma
+
+import android.view.LayoutInflater
+
+class ActivityMainBinding {
+    val textView: Any
+        get() {
+            TODO()
+        }
+    val editTextNumber: Any
+        get() {
+            TODO()
+        }
+    val button: Any
+        get() {
+            TODO()
+        }
+    val root: Any
+        get() {
+            TODO()
+        }
+
+    companion object {
+        fun inflate(layoutInflater: LayoutInflater) {
+
+        }
+    }
+
+}
